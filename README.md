1bb3a9f8c8dcc79c49755500d3cf0385868b9db3
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-09-23 16:28:02 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# linter-lesshint
+
+This plugin for linter provides an interface to [lesshint](https://github.com/lesshint/lesshint). It will lint your `.less` while editing and on save so you'll instantly see if your code is sticking your code conventions/style.
+
+## Installation
+```bash
+$ apm install linter-lesshint
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+'use babel';
+
+import path from 'path';
+import configLoader from 'lesshint/lib/config-loader';
+import helper from 'atom-linter';
+
+export default class LinterLesshint {
+    static config = {
+        onlyWithRc: {
+            default: false,
+            title: 'Disable linter when no `.lesshintrc` is found in project.',
+            type: 'boolean',
+        },
+    }
+
+    static get onlyWithRc() {
+        return atom.config.get('linter-lesshint.onlyWithRc');
+    }
+
+    static activate() {
+        require('atom-package-deps').install('linter-lesshint');
+    }
+
+    static provideLinter() {
+        return {
+            grammarScopes: ['source.css.less'],
+            scope: 'file',
+            lintOnFly: true,
+            lint: (editor) => {
+                const Lesshint = require('lesshint');
+                const lesshint = new Lesshint();
+
+                const text = editor.getText();
+                const filePath = editor.getPath();
+                const configFile = helper.findFile(path.dirname(filePath), '.lesshintrc');
+
+                if (!configFile && this.onlyWithRc) {
+                    return [];
+                }
+
+                let config = {};
+                try {
+                    if (configFile) {
+                        config = configLoader(configFile);
+                    }
+                } catch (e) {
+                    atom.notifications.addError("Something's wrong with the `.lesshintrc` file.", {
+                        dismissable: true,
+                    });
+                }
+
+                lesshint.configure(config);
+
+                const errors = lesshint.checkString(text, filePath);
+
+                return errors.map(({ linter, message, line, column, severity }) => {
+                    line = line || editor.getLineCount();
+
+                    const range = helper.rangeFromLineNumber(editor, line - 1, column);
+
+                    const type = severity;
+                    const html = `<span class='badge badge-flexible'>${linter}</span> ${message}`;
+
+                    return { type, html, filePath, range };
+                });
+            }
+        }
+    }
+}
